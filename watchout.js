afd79e4e6700c4ce335e467e30cd061a3d5a94e6
Adam Deibert <ardeibert@gmail.com>
2014-03-08 02:47:32 -0800

    generating player dot and onClick event

--- watchout.js
+++ watchout.js
@@ -1,19 +1,52 @@
 // start slingin' some d3 here.
 
-var height = window.innerHeight;
-var width = window.innerWidth;
+var height = 700; //parseInt(d3.select('svg.arena').style('height').replace("px",""));
+var width = 700; //parseInt(d3.select('svg.arena').style('width').replace("px",""));
+var heroStats = [{r: 10, fill: 'red', cy: 350, cx: 350}];
 
-var enemies = d3.select('svg').selectAll('circle').data([10,30,40,20]).enter().append('circle')
-.attr('r', function(d){ return d})
-.attr('fill', 'blue')
-.attr('cy', function() { return Math.floor(Math.random() * height)})
-.attr('cx', function() { return Math.floor(Math.random() * width)});
+var makeHero = function(heroStats){
+  var hero = d3.select('svg').selectAll('circle.hero').data(heroStats);
+  
+  hero.enter().append('svg:circle')
+    .attr({
+      'r': function(d){ return d.r },
+      'fill': function(d){ return d.fill },
+      'cy': function(d){ return d.cy },
+      'cx': function(d){ return d.cx },
+      'class': '.hero'
+    });
+  return hero;
+};
 
-//Got lazy and made them transition to r: 30 instead of r: random
+
+var hero = makeHero(heroStats);
+
+var makeEnemies = function(x){
+  circles = [];
+  for(var i=0; i<x; i++){
+    circles.push({
+      r: 15,
+      fill: 'blue',
+      cy: Math.floor(Math.random() * (height - 30 + 1)) + 15,
+      cx: (Math.floor(Math.random() * (width - 30 + 1)) + 15)
+    })
+  }
+  return circles;
+};
+
+var enemies = d3.select('svg').selectAll('circle').data(makeEnemies(10)).enter().append('circle')
+  .attr({
+    'r': function(d){ return d.r },
+    'fill': 'blue',
+    'cy': function(d) { return d.cy },
+    'cx': function(d) { return d.cx }
+  });
+
 setInterval(function(){
   enemies.transition()
-  .attr('r', 30)
+  // .ease('bounce')
+  .duration(1300)
   .attr('fill', 'blue')
-  .attr('cy', function() { return Math.floor(Math.random() * height)})
-  .attr('cx', function() { return Math.floor(Math.random() * width)});
-}, 500);
+  .attr('cy', function() { return Math.floor(Math.random() * (height - 40 +1)) + 20 })
+  .attr('cx', function() { return Math.floor(Math.random() * (width - 40 + 1)) + 20 });
+}, 1100);
