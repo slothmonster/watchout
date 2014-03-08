8402c127e75e0d7fc1d68d9491e0bc24d3d41527
Hack Reactor Students <students@hackreactor.com>
2014-03-07 20:50:57 -0800

    pre rendered frames of bouncy balls

--- watchout.js
+++ watchout.js
@@ -1,22 +1,112 @@
-// start slingin' some d3 here.
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 var height = window.innerHeight;
 var width = window.innerWidth;
 
-//var game = d3.select('body').append('svg').attr({'height':height, 'width': width});
+window.addEventListener('resize', function(){
+  height = window.innerHeight;
+  width = window.innerWidth;
+});
 
 
-var opponents = d3.select('svg').selectAll('circle').data(d3.range(20)).enter().append('circle')
-  .attr('r', function(d) { return d*5; })
-  .attr('cx', function(d){ return Math.floor((Math.random() * width));})
-  .attr('cy', function(d){ return Math.floor((Math.random() * height));})
-  .attr('opacity', '0.5')
-  .attr('fill', 'blue');
+var currentFrame = [];
 
-window.setInterval(function(){
-  opponents.transition()
-    .attr('cx', function(d){ return Math.floor( (Math.random() * (width - (10*d)) + (d*5) ) );})
-    .attr('cy', function(d){ return Math.floor( (Math.random() * (height - (10*d)) + (d*5) ) );})
-    .attr('fill', function(d){
-      return '#' + Math.floor(Math.random() * 16777215).toString(16);
+for(var i = 0; i < 100; i++){
+  currentFrame.push({
+    r : 20,
+    cx : (Math.random() * (width - (40)) + 20 ),
+    cy : (Math.random() * (height - (40)) + 20 ),
+    xspeed : 5, //Math.ceil(Math.random() * 5),
+    yspeed : 5 //Math.ceil(Math.random() * 5)
+  });
+}
+var frames = [currentFrame];
+
+var generateFrame = function(thisFrame){
+
+  if(frames.length < 1000){
+    var newFrame = thisFrame.map(function(opponentFrame){
+      var obj = {};
+      obj.r = opponentFrame.r;
+      obj.cx = opponentFrame.cx + opponentFrame.xspeed;
+      obj.cy = opponentFrame.cy + opponentFrame.yspeed;
+
+      if(obj.cx > width-20){
+        obj.xspeed = Math.abs(opponentFrame.xspeed) * -1;
+      } else if(obj.cx < 20){
+        obj.xspeed = Math.abs(opponentFrame.xspeed)
+      } else {
+        if(Math.abs(opponentFrame.xspeed) > 10){
+          obj.xspeed = opponentFrame.xspeed - ( (Math.random() * 1) * opponentFrame.xspeed/Math.abs(opponentFrame.xspeed)) ;
+        } else if(Math.abs(opponentFrame.xspeed) < 3){
+          obj.xspeed = opponentFrame.xspeed + ( (Math.random() * 1) * opponentFrame.xspeed/Math.abs(opponentFrame.xspeed));
+        } else {
+          obj.xspeed = opponentFrame.xspeed + (Math.random() * 0.5) - (Math.random() * 0.5);
+        }
+      }
+
+      if(obj.cy > height-20){
+        obj.yspeed = Math.abs(opponentFrame.yspeed) * -1;
+      } else if(obj.cy < 20){
+        obj.yspeed = Math.abs(opponentFrame.yspeed)
+      } else {
+        if(Math.abs(opponentFrame.yspeed) > 10){
+          obj.yspeed = opponentFrame.yspeed - ( (Math.random() * 1) * opponentFrame.yspeed/Math.abs(opponentFrame.yspeed));
+        } else if(Math.abs(opponentFrame.yspeed) < 3){
+          obj.yspeed = opponentFrame.yspeed + ( (Math.random() * 1) * opponentFrame.yspeed/Math.abs(opponentFrame.yspeed));
+        } else {
+          obj.yspeed = opponentFrame.yspeed + (Math.random() * 0.5) - (Math.random() * 0.5);
+        }
+      }
+
+      return obj;
     });
-}, 300);
+    frames.push(newFrame);
+
+    setTimeout(function(){
+      generateFrame(newFrame);
+    },5);
+
+  } else {
+    setTimeout(function(){
+      generateFrame(thisFrame);
+    },5);
+
+  }
+
+}
+
+generateFrame(currentFrame);
+
+
+var opponents = d3.select('svg').selectAll('circle').data(currentFrame).enter().append('circle')
+  .attr('r', function(d) { return d.r; })
+  .attr('cx', function(d){ return d.cx; })
+  .attr('cy', function(d){ return d.cy;})
+  .attr('opacity', '0.5')
+  .attr('fill', 'black');
+
+var render = function(){
+  opponents
+    .data(frames.shift())
+    .attr('r', function(d) { return d.r; })
+    .attr('cx', function(d){ return d.cx;})
+    .attr('cy', function(d){ return d.cy;});
+    // .attr('fill', function(d){
+    //   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // });
+};
+
+
+(function animloop(){
+  requestAnimFrame(animloop);
+  //setTimeout(animloop, 700);
+  render();
+})();
